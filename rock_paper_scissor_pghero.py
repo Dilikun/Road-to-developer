63efaa2202e980bd7f76d6ed268677d312af2a4d
dilik <dilik.rayli@gmail.com>
2023-12-03 17:09:01 +0500

    programming hero lessons second simple game rock paper and scissors

--- rock_paper_scissor_pghero.py
+++ rock_paper_scissor_pghero.py
@@ -23,3 +23,26 @@ def get_winner(p1, p2):
 player1 = input('First player: rock, paper or scissors: ')
 player2 = input('First player: rock, paper or scissors: ')
 print(get_winner(player1, player2))
+
+
+# Alternative solution
+def get_winner_second(p1,p2):
+    if p1 == "rock" and p2 == "paper":
+        return 'Second player wins!'
+    elif p1 == "rock" and p2 == "scissors":
+        return 'First player wins!'
+    elif p1 == "paper" and p2 == "rock":
+        return 'First player wins!'
+    elif p1 == "paper" and p2 == "scissors":
+        return 'Second player wins!'
+    elif p1 == "scissors" and p2 == "rock":
+        return 'Second player wins!'
+    elif p1 == "scissors" and p2 == "paper":
+        return 'First player wins!'
+    else:
+        return 'Tie'
+
+
+player11 = input('First player: rock, paper or scissors: ')
+player12 = input('First player: rock, paper or scissors: ')
+print(get_winner_second(player11, player12))
