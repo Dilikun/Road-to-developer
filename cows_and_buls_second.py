82d3f57f9f5a65ece80e4db2d581847cdf0b242a
dilik <dilik.rayli@gmail.com>
2023-12-04 17:43:13 +0500

    programming hero lessons second simple game cows and bulls version 2

--- cows_and_buls_second.py
+++ cows_and_buls_second.py
@@ -2,12 +2,12 @@ import random
 
 secret_numbers = str(random.randint(1000, 9999))
 print('Try guess number. It contains 4 digits')
-
+# print(secret_numbers)
 remaining_try = 7
 
 
 def get_bulls_cows(number, user_guess):
-    bulls_cows = []
+    bulls_cows = [0, 0]
     for i in range(len(number)):
         if user_guess[i] == number[i]:
             bulls_cows[0] += 1
@@ -24,7 +24,7 @@ while remaining_try > 0:
         print('YOU WIN!')
         break
     else:
-        bulls_cows = get_bulls_cows(secret_numbers,player_guess)
+        bulls_cows = get_bulls_cows(secret_numbers, player_guess)
 
         print('Bulls is: ', bulls_cows[0])
         print('Cows is: ', bulls_cows[1])
