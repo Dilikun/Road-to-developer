e3506c286d7d0760a756b3ea42906dc1f78495e8
dilik <dilik.rayli@gmail.com>
2023-12-05 19:33:12 +0500

    programming hero course simple game projects - simple sentence generator

---
+++ generate_sentence.py
@@ -0,0 +1,7 @@
+subjects = ['i', 'You']
+verbs = ['Play', 'Love']
+objects = ['Hockey', 'Basketball']
+for i in range(len(subjects)):
+    for j in range(len(verbs)):
+        for k in range(len(verbs)):
+            print(subjects[i], verbs[j],objects[k])
