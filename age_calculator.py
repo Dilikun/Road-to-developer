57342e09b09fa60f0f95135cb2491ffb938405c7
dilik <dilik.rayli@gmail.com>
2023-12-07 17:05:37 +0500

    programming hero course simple game projects - simple age calculator

---
+++ age_calculator.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import time
+
+def calculate_age(born):
+    today = datetime.today()
+    days = (today - born).days
+    age = days//365
+    return age
+
+def get_user_birthday():
+    date_str = input('Enter our birthdate in DD/MM/YYYY format: ')
+    try:
+        birthday = datetime.strptime(date_str, '%d/%m/%Y')
+    except TypeError:
+        birthday = datetime.datetime(*(time.strptime(date_str, '%d/%m/%Y')[0:6]))
+    return birthday
+
+
+birthday = get_user_birthday()
+age = calculate_age(birthday)
+print('Your age is: ', age)
